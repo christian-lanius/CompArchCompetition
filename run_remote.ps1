ba7cf4dfb860213a212eebd5323504eef84e933c
Christian Lanius <christian.lanius@rwth-aachen.de>
2018-07-11 11:49:13 +0900

    capable of running on linux

---
+++ run_remote.ps1
@@ -0,0 +1,3 @@
+﻿scp -r C:\Users\christian\Documents\git_files\CompArchCompetition ex2023@comparc01.am.ics.keio.ac.jp:~/
+ 
+ssh -t ex2023@comparc01.am.ics.keio.ac.jp "cd ~/CompArchCompetition ;export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH; make clean; make; ./conv"
